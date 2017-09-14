82abad9d67885d078b09782bf65aafa0f3662139
GokulPo <youremail@domain.com>
2017-09-14 14:20:04 +0530

    Add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,11 +17,15 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)    # Not the final implementation!
+    @user = User.new(user_params)
       if @user.save
-        log_in @user
-        flash[:success] = "Welcome to the Sample App!"
-        redirect_to @user
+        #log_in @user
+        #flash[:success] = "Welcome to the Sample App!"
+        #UserMailer.account_activation(@user).deliver_now
+        @user.send_activation_email
+        flash[:info] = "Please check your email to activate your account."
+        redirect_to root_url
+        #redirect_to @user
       else
         render 'new'
       end
--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,5 @@ Rails.application.routes.draw do
   delete '/logout',  to: 'sessions#destroy'
 
    resources :users
+   resources :account_activation, only: [:edit]
 end
